e506d1a876cd74664f4a57915288cfcbde1e7e18
KeenCat <hmkim8011@naver.com>
2016-05-29 02:10:10 +0900

    Make Better Quit Menu
    1. Press START button -> Go to Game Scene.
    2. Press QUIT button -> Pop up Want_Quit window -> Press Yes button ->
    Quit Application // Press No button -> Return Menu window

--- Solomon_judgement/Assets/InstantGui/Scripts/Elements/InstantGuiButton.cs
+++ Solomon_judgement/Assets/InstantGui/Scripts/Elements/InstantGuiButton.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class InstantGuiButton : InstantGuiElement
 {
 	public InstantGuiActivator onPressed;
-	
-	public override void Action ()
+
+    public override void Action ()
 	{
 		base.Action();
-		if (activated) onPressed.Activate(this);
+        if (activated)
+        {
+            onPressed.Activate(this);
+            if (base.name == "START") { SceneManager.LoadScene("Game"); }
+            else if (base.name == "Quit_Yes") { Application.Quit(); }
+            
+        }
 	}
 }
