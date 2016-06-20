a246f373698456181d7187f4e372222e156e20a5
ahroobe <vivian2323@naver.com>
2016-06-20 13:33:45 +0900

    game pause
    if we press pause button, game is paused.

--- Solomon_judgement/Assets/InstantGui/Scripts/Elements/InstantGuiButton.cs
+++ Solomon_judgement/Assets/InstantGui/Scripts/Elements/InstantGuiButton.cs
@@ -12,10 +12,18 @@ public class InstantGuiButton : InstantGuiElement
         if (activated)
         {
             onPressed.Activate(this);
-            if (base.name == "START") { SceneManager.LoadScene("Game"); }
+            if (base.name == "START") { 
+				SceneManager.LoadScene("Game");
+				Time.timeScale = 1;
+				}
             else if (base.name == "Quit_Yes") { Application.Quit(); }
             else if (base.name == "Quit_Start") { SceneManager.LoadScene("GUIscene"); }
-            
+			if (base.name == "Pause_Button") {
+				Time.timeScale = 0;
+			}
+			if (base.name == "Resume") {
+				Time.timeScale = 1;
+			}
         }
 	}
 }
