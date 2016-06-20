965eee60c2f60a34b14d9e66e5c268d8c95daaf8
KeenCat <hmkim8011@naver.com>
2016-06-20 17:27:44 +0900

    collider process
    1. if object collides with wall -> destroy

---
+++ Solomon_judgement/Assets/PlainController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+      GameObject[] see;
+
+            see = GameObject.FindGameObjectsWithTag(other.gameObject.tag);
+            foreach (GameObject se in see)
+            {
+                Destroy(se);
+            }
+      
+        
+    }
+
+}
--- Solomon_judgement/Assets/_Scripts/SpawnHumans.cs
+++ Solomon_judgement/Assets/_Scripts/SpawnHumans.cs
@@ -8,24 +8,88 @@ public class SpawnHumans : MonoBehaviour {
 
     public GameObject[] Humans;
     //public GameObject[] Coins;
+    public GameObject fakeBox;
+    int couples = 0;
+    int solos = 0;
+    int obstacles = 0;
+    string[] coupletag = { "couple1", "couple2", "couple3", "couple4", "couple5" };
+    string[] solotag = { "solo1", "solo2", "solo3", "solo4"};
+    string[] obstag = { "bin1", "bin2", "bin3", "bin4" };
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         int TimeIndex = Random.Range(0, spawnTime.Length);
 
-        InvokeRepeating("SpawnHuman", 1.5f, spawnTime[TimeIndex]);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+        InvokeRepeating("SpawnHuman", 1.3f, spawnTime[TimeIndex]);
+        couples = 0;
+        solos = 0;
+        obstacles = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     void SpawnHuman()
     {
+
         int spawnIndex = Random.Range(0, SpawnPoints.Length);//set index number of array randomly.
         int HumanIndex = Random.Range(0, Humans.Length);
-        Instantiate(Humans[HumanIndex], SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+        GameObject gamechar = (GameObject)Instantiate(Humans[HumanIndex], SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+        GameObject fakeBox2 = (GameObject) Instantiate(fakeBox, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+        if ((HumanIndex % 2 == 0))
+        {
+            string tag1 = coupletag[couples];
+            gamechar.tag = tag1;
+            fakeBox2.tag = tag1;
+            if (couples == 4)
+            {
+                couples = 0;
+            }else
+            {
+                couples = couples + 1;
+            }
+       
+
+        } else if (HumanIndex % 2 == 1 && HumanIndex < 6)
+        {
+            string tag2 = solotag[solos];
+            gamechar.tag = tag2;
+            fakeBox2.tag = tag2;
+            if (solos == 3)
+            {
+                solos = 0;
+            }else
+            {
+                solos = solos + 1;
+            }
+        } else
+        {
+            string tag3 = obstag[obstacles];
+            gamechar.tag = tag3;
+            fakeBox2.tag = tag3;
+            if (obstacles == 3)
+            {
+                obstacles = 0;
+            }
+            else
+            {
+                obstacles = obstacles + 1;
+            }
+        }
+        /*
+        Rigidbody rigd = fakeBox.AddComponent<Rigidbody> ();
+        rigd.isKinematic = true;
+        rigd.useGravity = true;
+        //rigd.velocity = transform.TransformDirection(Vector3.back * 10);
+        CapsuleCollider capsule = rigd.gameObject.AddComponent<CapsuleCollider>();
+        
+        //capsule.direction = 2;
+        //humanrigd = capsule.attachedRigidbody;
+        capsule.radius = 1.0f;
+        capsule.height = 2.0f;
+        capsule.isTrigger = true;
+        */
     }
 }
